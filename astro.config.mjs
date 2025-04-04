e6b001a5ced91e818a0ba5dec945dc95af1d786d
SergioLKG <sergiodp.dev@gmail.com>
2025-04-05 00:32:16 +0200

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sergiodominguez.vercel.app',
   output: 'server',
-  adapter: vercel(),
   integrations: [
     tailwind(),
     react(),
