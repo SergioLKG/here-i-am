5b285375f2c846966efeb02d4387516e29a9d215
SergioLKG <sergiodp.dev@gmail.com>
2025-04-05 00:38:09 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sergiodominguez.vercel.app',
   output: 'server',
+
   integrations: [
     tailwind(),
     react(),
@@ -20,12 +23,14 @@ export default defineConfig({
       }
     })
   ],
+
   i18n: {
     defaultLocale: 'es',
     locales: ['en', 'es'],
     routing: {
       prefixDefaultLocale: true
     }
-  }
-});
+  },
 
+  adapter: vercel()
+});
